e3428317ac9754c536b5a2e297227f3f3efa3e1a
Harishkumar Muddasani <harishmuddasa@gmail.com>
2023-06-16 04:48:03 -0500

    add comment to main method

--- src/main/java/io/yelpcode/todoappdemo/SpringBootTodoAppDemoApplication.java
+++ src/main/java/io/yelpcode/todoappdemo/SpringBootTodoAppDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootTodoAppDemoApplication {
 
+	//add comment1
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTodoAppDemoApplication.class, args);
 	}
